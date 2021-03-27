e8b24290ab253f922a2974cb149a4666f3501a2f
Eric Butera <eric.butera@gmail.com>
2021-03-27 08:19:13 -0400

    triangle

--- triangle.rb
+++ triangle.rb
@@ -14,7 +14,14 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+
+  # all sides equal
+  return :equilateral if a == b && b == c
+
+  # exactly 2 sides are equal
+  return :isosceles if a == b || a == c || b == c
+
+  :scalene
 end
 
 # Error class used in part 2.  No need to change this code.
